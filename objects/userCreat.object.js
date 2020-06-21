1b20906876acfd8cfe0da8c953a1cc5991066409
tuananh02088 <anh.huynh0209@outlook.com>
2020-06-22 01:41:59 +0700

    Revert "Revert "tuan anh add file""
    This reverts commit 0d1389258ee4c6d7f36564ee9ab1dc5dedc25315.

---
+++ objects/userCreat.object.js
@@ -0,0 +1,10 @@
+class userCreat {
+    constructor(name, id, pass, phone, avatar) {
+        this.name = name;
+        this.pass = pass;
+        this.phone = phone;
+        this.avatar = avatar;
+    }
+};
+
+module.exports = userCreat
