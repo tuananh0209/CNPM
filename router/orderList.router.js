833abd02e684d61177b8b06545a34cb00ae27fbe
HuaThiSon <65495338+HuaThiSon@users.noreply.github.com>
2020-06-25 23:02:06 +0700

    Add files via upload

--- router/orderList.router.js
+++ router/orderList.router.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express')
 const controller = require('../controllers/orderList.controller')
 
@@ -10,17 +9,4 @@ router.get('/:id', controller.viewOrders);
 
 // router.get('/:id', controller.view);
 
-=======
-const express = require('express')
-const controller = require('../controllers/orderList.controller')
-
-const router = express.Router()
-
-router.get('/orderList', controller.orderList);
-
-router.get('/:id', controller.viewOrders);
-
-// router.get('/:id', controller.view);
-
->>>>>>> de14f9a761f0579b9b0d49696f5b5f00e61319dc
 module.exports = router
