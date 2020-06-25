9ea63496db91c28be8f3ab5a19ddba97f689fae4
HuaThiSon <65495338+HuaThiSon@users.noreply.github.com>
2020-06-25 23:03:37 +0700

    Add files via upload

--- models/orderList.model.js
+++ models/orderList.model.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var mongoose = require('mongoose');
 
 var orderList = new mongoose.Schema({
@@ -21,28 +20,4 @@ var orderList = new mongoose.Schema({
 });
 var orderList = mongoose.model('orderList', orderList, 'orderList');
 
-=======
-var mongoose = require('mongoose');
-
-var orderList = new mongoose.Schema({
-    note: {
-        type: String,
-        require: true
-    },
-    cart: {
-        type: Object,
-        require: true
-    },
-    userId: {
-        type: String,
-        require: true
-    },
-    time :{
-        type : String,
-        require : true
-    }
-});
-var orderList = mongoose.model('orderList', orderList, 'orderList');
-
->>>>>>> de14f9a761f0579b9b0d49696f5b5f00e61319dc
 module.exports = orderList;
